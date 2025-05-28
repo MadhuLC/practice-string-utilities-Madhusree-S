9da3f710bf3855bec15ff7c9431e6c9268e95d24
Madhusree Sakhamuri <madhusree.bellamkonda@gmail.com>
2025-05-28 08:45:16 -0400

    stringUtilities

---
+++ string-utilities/stringUtils.js
@@ -0,0 +1,19 @@
+// stringUtils.js
+
+function capitalize(str) {
+  if (typeof str !== 'string' || !str.length) return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+function reverse(str) {
+  if (typeof str !== 'string') return '';
+  return str.split('').reverse().join('');
+}
+function contains(str, substr) {
+  if (typeof str !== 'string' || typeof substr !== 'string') return false;
+  return str.includes(substr);
+}
+module.exports = {
+  capitalize,
+  reverse,
+  contains,
+}
